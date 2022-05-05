fa414333bd597d60aa6c049a4e58d732b33bd409
Samir.Sayyed <you@example.com>
2022-05-05 08:51:18 +0530

    first commit

---
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -0,0 +1,34 @@
+package com.example.android.marsphotos
+
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.PhotoGridAdapter
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?){
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        imgView.load(imgUri) {
+            placeholder(R.drawable.loading_animation)
+//            kotlin.error(R.drawable.ic_broken_image)
+        }
+    }
+}
+
+
+
+class BindingAdapters {
+
+}
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView,
+                     data: List<MarsPhoto>?) {
+
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
